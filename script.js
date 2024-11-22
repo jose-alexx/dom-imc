7186afbb276abd61638e2da57b63c5e207fbff30
jose alex <josealex41012@gmail.com>
2024-11-22 13:50:24 -0300

    add

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ resultado.innerHTML = ""; /** Limpa o código */
 btnCalcular.addEventListener('click' , () => {
     console.log("Clicou!");
     const inputAltura =  document.querySelector("#inputAltura");
-    console.log(inputAltura.value);
+    // console.log(inputAltura.value);
     const inputPeso =  document.querySelector("#inputPeso");
-    console.log(inputPeso.value);
+    // console.log(inputPeso.value);
+
+    const peso = inputPeso.value;
+    const altura = inputAltura.value;
+    // console.log(altura*altura);
 
     // console.log(inputAltura.value)
     // console.log(inputPeso.value)
-
     resultado.classList.remove("hide");
-    const altura = Number(inputAltura.value);
-    const peso = Number(inputPeso.value);
 
+    const imc = peso / (altura * altura);
+    console.log(imc);
+
+
+    /** const altura = Number(inputAltura.value); */
+    /** const peso = Number(inputPeso.value); */
     
+    
+
+
 
 });
